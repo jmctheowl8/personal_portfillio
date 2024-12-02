e219a377ab4669c52720384728f76a1649601be3
jmctheowl8 <john.cabrera@lc.cuny.edu>
2024-12-02 16:57:35 -0500

    web app ready for heroku deployment

--- src/compoents/Hero/hero.jsx
+++ src/compoents/Hero/hero.jsx
@@ -2,7 +2,7 @@ import "./hero.css"
 const Hero = () => {
   return (
     <div className="hero">
-      <img src="../../../public/Images/openart-image_3yLFJKar_1731359493913_raw.jpg" alt="Hero" className="hero-img" />
+      <img src="/Images/openart-image_3yLFJKar_1731359493913_raw.jpg" alt="Hero" className="hero-img" />
       <div className="hero-text">
         <h2 className="hero-text">Software Engineering</h2>
         <h3 id="hero-qoute">&quot;Software Engineering Is not just about writing code.<br /> It&apos;s about solving problems and making lives better.&quot;</h3>
--- src/compoents/gallery/gallery.jsx
+++ src/compoents/gallery/gallery.jsx
@@ -4,13 +4,13 @@ const Gallery = () => {
   return (
     <div className="gallery">
         <div className="gallery-item">
-            <img src="../../../public/Images/project1.jpg" alt="Gallery Image 1" />
+            <img src="/Images/project1.jpg" alt="Gallery Image 1" />
         </div>
         <div className="gallery-item">
-            <img src="../../../public/Images/project 2.jpg" alt="Gallery Image 1" />
+            <img src="/Images/project 2.jpg" alt="Gallery Image 1" />
         </div>
         <div className="gallery-item">
-            <img src="../../../public/Images/project3.jpg" alt="Gallery Image 1" />
+            <img src="/Images/project3.jpg" alt="Gallery Image 1" />
         </div>
     </div>
 
