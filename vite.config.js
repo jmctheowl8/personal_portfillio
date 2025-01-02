966e2757c8fc3df729dadfb3720f0c1099a19059
jmctheowl8 <john.cabrera@lc.cuny.edu>
2025-01-02 15:37:01 -0500

    run import useing absolute path

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct asset paths when building for production
+  base: '/', // Ensures correct asset paths when building for production
   resolve: {
     alias: {
       '@': '/src',
+      '@styles': '/src/styles',
     },
   },
   
