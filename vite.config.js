3e8c728b820fe02c1dd6f398c89e8018afed348c
jmctheowl8 <john.cabrera@lc.cuny.edu>
2025-01-02 15:04:03 -0500

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensures correct asset paths when building for production
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  
 })
 
