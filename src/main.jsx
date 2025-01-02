2d519315bbc742f0d42c136a40ab4315bbb9d8ab
jmctheowl8 <john.cabrera@lc.cuny.edu>
2025-01-02 16:06:25 -0500

    noticed that path was different on github than local

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './src/styles/global.css'
+import './src/Styles/global.css'
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
